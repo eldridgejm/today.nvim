5b77ac43d65055cdbc13f44c1fdd2a52395c09b5
Justin Eldridge <eldridgejm@gmail.com>
2021-08-13 10:41:14 -0700

    exposed options in filterer

--- lua/today/core/filterers.lua
+++ lua/today/core/filterers.lua
@@ -6,23 +6,45 @@ local util = require('today.util')
 
 local filterers = {}
 
+function filterers.make_filterer(options, filter)
+
+    local Filterer = {
+        options = options
+    }
+
+    local meta = {}
+
+    function meta.__call(self, t) return filter(self, t) end
+
+    setmetatable(Filterer, meta)
+
+    return Filterer
+
+end
+
 --- Filters by tags.
 -- @param target_tags A list of the tags to include.
 function filterers.tag_filterer(target_tags)
-    return function(t)
+
+    local options = {
+        tags = target_tags
+    }
+
+    return filterers.make_filterer(options,
+    function(self, t)
         local task_tags = task.get_tags(t)
         for _, tag in pairs(task_tags) do
-            if util.contains_value(target_tags, tag) then
+            if util.contains_value(self.options.tags, tag) then
                 return true
             end
         end
 
-        if (#task_tags == 0) and util.contains_value(target_tags, "none") then
+        if (#task_tags == 0) and util.contains_value(self.options.tags, "none") then
             return true
         end
 
         return false
-    end
+    end)
 end
 
 
