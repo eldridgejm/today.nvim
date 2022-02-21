f5fb914c1aefc135f036f06ca009b1dc99844821
Justin Eldridge <eldridgejm@gmail.com>
2022-02-20 21:25:16 -0800

    Removed inference from taskparser

--- lua/today/core/test/inferrer_spec.lua
+++ lua/today/core/test/inferrer_spec.lua
@@ -3,23 +3,27 @@ describe("today.core.inferrer", function()
     local Task = require("today.core.task").Task
     local DateObj = require("today.core.dates").DateObj
 
-    it("should replace a nil do date", function()
-        local header_info = {
-            kind = "do_date",
-            do_date = DateObj:new("2022-20-21")
-        }
+    describe("do_date header", function()
 
-        local task = Task:new({
-            done = true,
-            tags = {},
-            priority = 2,
-            description = "This is a test.",
-            do_date = nil,
-            recur_pattern = nil
-        })
+        it("should replace a nil do date", function()
+            local header_info = {
+                kind = "do_date",
+                do_date = DateObj:new("2022-20-21")
+            }
 
-        local task = inferrer.infer(header_info, task)
+            local task = Task:new({
+                done = true,
+                tags = {},
+                priority = 2,
+                description = "This is a test.",
+                do_date = nil,
+                recur_pattern = nil
+            })
 
-        assert.are.equal(task.do_date, DateObj:new("2022-20-21"))
+            local task = inferrer.infer(header_info, task)
+
+            assert.are.equal(task.do_date, DateObj:new("2022-20-21"))
+        end)
+
     end)
 end)
