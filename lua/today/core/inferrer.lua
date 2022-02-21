e23aa174122802b9acf01b7764df9dae93512ade
Justin Eldridge <eldridgejm@gmail.com>
2022-02-20 22:01:32 -0800

    Added inferred.

--- lua/today/core/inferrer.lua
+++ lua/today/core/inferrer.lua
@@ -1,8 +1,66 @@
+local util = require("today.core.util")
+local Task = require("today.core.task").Task
+local dates = require("today.core.dates")
+local DateObj = dates.DateObj
+
+
 local M = {}
 
+
+local function infer_do_date_from_header(task, header_info)
+    if task.do_date == nil then
+        task.do_date = header_info.do_date
+    end
+end
+
+
+local function infer_tag_from_header(task, header_info)
+    if util.index_of(task.tags, header_info.tag) ~= nil then
+        -- the tag already appears, so exit
+        return
+    end
+    table.insert(task.tags, 1, header_info.tag)
+end
+
+local function infer_done_from_header(task)
+    task.done = true
+end
+
 --- Given the header info and the task, modify the task as appropriate
-function M.infer(header_info, task)
-    task.do_date = header_info.do_date
+function M.infer_from_header(task, header_info)
+    task = Task:new(task)
+
+    if header_info.kind == "do_date" then
+        infer_do_date_from_header(task, header_info)
+    elseif header_info.kind == "tag" then
+        infer_tag_from_header(task, header_info)
+    elseif header_info.kind == "done" then
+        infer_done_from_header(task)
+    end
+
+    return task
+end
+
+
+function M.infer_defaults(task, working_date)
+    task = Task:new(task)
+    working_date = DateObj:new(working_date)
+
+    if task.done == nil then
+        task.done = false
+    end
+
+    if task.priority == nil then
+        task.priority = 0
+    end
+
+    if task.do_date == nil and task.recur_pattern == nil then
+        task.do_date = working_date
+    elseif task.do_date == nil and task.recur_pattern ~= nil then
+        -- assign a "next in sequence" do date
+        task.do_date = dates.next(working_date:add_days(-1), task.recur_pattern)
+    end
+
     return task
 end
 
--- lua/today/core/test/inferrer_spec.lua
+++ lua/today/core/test/inferrer_spec.lua
@@ -3,27 +3,237 @@ describe("today.core.inferrer", function()
     local Task = require("today.core.task").Task
     local DateObj = require("today.core.dates").DateObj
 
-    describe("do_date header", function()
+    describe("infer_from_header", function()
 
-        it("should replace a nil do date", function()
-            local header_info = {
-                kind = "do_date",
-                do_date = DateObj:new("2022-20-21")
-            }
+        describe("do_date header", function()
 
+            it("should replace a nil do date", function()
+                local header_info = {
+                    kind = "do_date",
+                    do_date = DateObj:new("2022-20-21")
+                }
+
+                local task = Task:new({
+                    done = true,
+                    tags = {},
+                    priority = 2,
+                    description = "This is a test.",
+                    do_date = nil,
+                    recur_pattern = nil
+                })
+
+                local task = inferrer.infer_from_header(task, header_info)
+
+                assert.are.equal(task.do_date, DateObj:new("2022-20-21"))
+            end)
+
+            it("should not replace a non-nil do date", function()
+                local header_info = {
+                    kind = "do_date",
+                    do_date = DateObj:new("2022-20-21")
+                }
+
+                local task = Task:new({
+                    done = true,
+                    tags = {},
+                    priority = 2,
+                    description = "This is a test.",
+                    do_date = DateObj:new("2000-01-01"),
+                    recur_pattern = nil
+                })
+
+                local task = inferrer.infer_from_header(task, header_info)
+
+                assert.are.equal(task.do_date, DateObj:new("2000-01-01"))
+            end)
+
+            it("should not replace existing recur pattern", function()
+                local header_info = {
+                    kind = "do_date",
+                    do_date = DateObj:new("2022-20-21")
+                }
+
+                local task = Task:new({
+                    done = true,
+                    tags = {},
+                    priority = 2,
+                    description = "This is a test.",
+                    do_date = nil,
+                    recur_pattern = "every 3 days"
+                })
+
+                local task = inferrer.infer_from_header(task, header_info)
+
+                assert.are.equal(task.do_date, DateObj:new("2022-20-21"))
+                assert.are.equal(task.recur_pattern, "every 3 days")
+            end)
+
+        end)
+
+        describe("tag header", function()
+            it("should add the tag to beginning if it doesn't already appear in the task", function()
+                local header_info = {
+                    kind = "tag",
+                    tag = "foo"
+                }
+
+                local task = Task:new({
+                    done = true,
+                    tags = {"one", "two"},
+                    priority = 2,
+                    description = "This is a test.",
+                    do_date = nil,
+                    recur_pattern = "every 3 days"
+                })
+
+                local task = inferrer.infer_from_header(task, header_info)
+
+                assert.are.same(task.tags, {"foo", "one", "two"})
+            end)
+
+            it("should not add the tag if it already appears", function()
+                local header_info = {
+                    kind = "tag",
+                    tag = "foo"
+                }
+
+                local task = Task:new({
+                    done = true,
+                    tags = {"foo", "bar"},
+                    priority = 2,
+                    description = "This is a test.",
+                    do_date = nil,
+                    recur_pattern = "every 3 days"
+                })
+
+                local task = inferrer.infer_from_header(task, header_info)
+
+                assert.are.same(task.tags, {"foo", "bar"})
+            end)
+        end)
+
+        describe("done category", function()
+            it("should check the task as done if is undone", function()
+                local header_info = {
+                    kind = "done",
+                }
+
+                local task = Task:new({
+                    done = false,
+                    tags = {"foo", "bar"},
+                    priority = 2,
+                    description = "This is a test.",
+                    do_date = nil,
+                    recur_pattern = "every 3 days"
+                })
+
+                local task = inferrer.infer_from_header(task, header_info)
+
+                assert.are.same(task.done, true)
+            end)
+
+            it("should check the task as done if done is nil", function()
+                local header_info = {
+                    kind = "done",
+                }
+
+                local task = Task:new({
+                    done = nil,
+                    tags = {"foo", "bar"},
+                    priority = 2,
+                    description = "This is a test.",
+                    do_date = nil,
+                    recur_pattern = "every 3 days"
+                })
+
+                local task = inferrer.infer_from_header(task, header_info)
+
+                assert.are.same(task.done, true)
+            end)
+        end)
+    end)
+
+    describe("infer_defaults", function()
+
+        it("should assign a default done of false", function()
             local task = Task:new({
-                done = true,
-                tags = {},
+                done = nil,
+                tags = {"foo", "bar"},
                 priority = 2,
+                description = "This is a test.",
+                do_date = nil,
+                recur_pattern = "every 3 days"
+            })
+
+            local task = inferrer.infer_defaults(task, "2022-02-20")
+
+            assert.are.same(task.done, false)
+        end)
+
+        it("should assign a default priority of 0", function()
+            local task = Task:new({
+                done = nil,
+                tags = {"foo", "bar"},
+                priority = nil,
+                description = "This is a test.",
+                do_date = nil,
+                recur_pattern = "every 3 days"
+            })
+
+            local task = inferrer.infer_defaults(task, "2022-02-20")
+
+            assert.are.same(task.priority, 0)
+        end)
+
+        it("should assign a do_date of today if both do_date and recur are nil", function()
+            local task = Task:new({
+                done = nil,
+                tags = {"foo", "bar"},
+                priority = nil,
                 description = "This is a test.",
                 do_date = nil,
                 recur_pattern = nil
             })
 
-            local task = inferrer.infer(header_info, task)
+            local task = inferrer.infer_defaults(task, "2022-02-20")
 
-            assert.are.equal(task.do_date, DateObj:new("2022-20-21"))
+            assert.are.same(task.do_date, DateObj:new("2022-02-20"))
+            assert.are.same(task.recur_pattern, nil)
+        end)
+
+        it("should leave things alone if do_date is provided by recur is nil", function()
+            local task = Task:new({
+                done = nil,
+                tags = {"foo", "bar"},
+                priority = nil,
+                description = "This is a test.",
+                do_date = DateObj:new("2022-02-01"),
+                recur_pattern = nil
+            })
+
+            local task = inferrer.infer_defaults(task, "2022-02-20")
+
+            assert.are.same(task.do_date, DateObj:new("2022-02-01"))
+            assert.are.same(task.recur_pattern, nil)
+        end)
+
+        it("should assign a next in sequence do_date if none provided and recur is given", function()
+            local task = Task:new({
+                done = nil,
+                tags = {"foo", "bar"},
+                priority = nil,
+                description = "This is a test.",
+                do_date = nil,
+                recur_pattern = "every wed"
+            })
+
+            -- 2022-02-20 is a sunday
+            local task = inferrer.infer_defaults(task, "2022-02-20")
+
+            assert.are.same(task.do_date, DateObj:new("2022-02-23"))
+            assert.are.same(task.recur_pattern, "every wed")
         end)
 
     end)
+
 end)
